692219ffefe982473030448f4f976228835e402d
Dan Richards <dan.m.richards@gmail.com>
2018-03-19 22:19:48 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# From Zero to Hero
+Here we have a small application designed to illustrate the zero values of
+various types in Go. It'll also show you some simple usage of pointers.
+
+## Usage
+Simply run the application like so:
+```golang
+go run main.go
+```
---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import "fmt"
+
+func simpleTypes() {
+	// Here we have an integer variable. What do you think the zero value
+	// of an integer is going to be? What would happen if we used a different
+	// type of integer?
+	var i int
+	fmt.Printf("integer: %v\n", i)
+
+	// Here we have a 64 bit float variable. What do you think the zero value
+	// of a float is going to be? What would happen if we used a different
+	// type of float?
+	var f float64
+	fmt.Printf("float: %v\n", f)
+
+	// Here we have a boolean variable. What do you think the zero value
+	// of a boolean is going to be?
+	var b bool
+	fmt.Printf("boolean: %v\n", b)
+
+	// Here we have a string variable. What do you think the zero value
+	// of a string is going to be?
+	var s string
+	fmt.Printf("string: %q\n", s)
+}
+
+func moarSimpleTypes() {
+	// Here we have an integer variable. What do you think will be displayed
+	// when we use the & syntax?
+	var i int
+	fmt.Printf("integer: %v\n", &i)
+
+	// Here we have a 64 bit float variable. What do you think will be displayed
+	// when we use the & syntax?
+	var f float64
+	fmt.Printf("float: %v\n", &f)
+
+	// Here we have a boolean variable. What do you think will be displayed
+	// when we use the & syntax?
+	var b bool
+	fmt.Printf("boolean: %v\n", &b)
+
+	// Here we have a string variable. What do you think will be displayed
+	// when we use the & syntax?
+	var s string
+	fmt.Printf("string: %v\n", &s)
+}
+
+func pointers() {
+	// Here we have pointers to the same types we used above. Can you guess what
+	// the zero values will be in this case?
+	var i *int
+	var f *float64
+	var b *bool
+	var s *string
+
+	fmt.Printf("integer: %v, float: %v, boolean: %v, string: %v\n", i, f, b, s)
+}
+
+func moarPointers() {
+	// Here we've created a closure which takes a pointer to an integer and sets
+	// the pointed value to the value of a zero valued integer variable.
+	reset := func(i *int) {
+		var j int
+		*i = j
+	}
+
+	// We've defined an integer variable with an initial value of 1. We now pass
+	// the integer to the zero pointer function.
+	i := 1
+	reset(&i)
+
+	// What will this output?
+	fmt.Printf("integer: %v\n", i)
+}
+
+func main() {
+	simpleTypes()
+
+	fmt.Println()
+
+	moarSimpleTypes()
+
+	fmt.Println()
+
+	pointers()
+
+	fmt.Println()
+
+	moarPointers()
+}
